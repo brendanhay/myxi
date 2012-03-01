428153167d8bcd063236f392d199aea73276de63
brendanhay <brendan.g.hay@gmail.com>
2012-03-01 16:59:03 +0100

    Using erlang amqp client to establish a single direct connection

--- apps/amqpoxy/src/poxy_reader.erl
+++ apps/amqpoxy/src/poxy_reader.erl
@@ -1,7 +1,7 @@
 -module(poxy_reader).
 
 -include("include/amqpoxy.hrl").
--include_lib("rabbit_common/include/rabbit_framing.hrl").
+-include_lib("amqp_client/include/amqp_client.hrl").
 
 %% API
 -export([start_link/4]).
@@ -17,18 +17,22 @@
 %% TCP connection timeout
 -define(TIMEOUT, 1000).
 
+%% Connection tuning
 -define(FRAME_MAX_SIZE, 131072).
 
--record(v1, {sock              :: inet:socket(),
-             backend           :: node(),
-             stage = handshake :: poxy_parser:stage(),
-             protocol          :: rabbit_framing:protocol() | undefined,
-             info              :: poxy_parser:payload_info() | undefined,
-             proxy    = ""     :: string(),
-             pending  = false  :: boolean(),
-             buf      = []     :: iolist(),
-             buf_len  = 0      :: non_neg_integer(),
-             recv_len = ?HANDSHAKE :: non_neg_integer()}).
+-record(v1, {sock                  :: inet:socket(),
+             backend               :: node(),
+             stage = handshake     :: poxy_parser:stage(),
+             protocol              :: rabbit_framing:protocol() | undefined,
+             info                  :: poxy_parser:payload_info() | undefined,
+             proxy    = ""         :: string(),
+             pending  = false      :: boolean(),
+             buf      = []         :: iolist(),
+             buf_len  = 0          :: non_neg_integer(),
+             recv_len = ?HANDSHAKE :: non_neg_integer(),
+             user                  :: binary() | undefined,
+             conn                  :: pid() | undefined,
+            chan                   :: pid() | undefined }).
 
 %%
 %% API
@@ -49,7 +53,7 @@ start_link(Listener, Sock, cowboy_tcp_transport, Opts) ->
 %% @hidden
 init(Listener, Sock, Opts) ->
     ok = cowboy:accept_ack(Listener),
-    read(next_stage(#v1{sock = Sock, proxy = amqpoxy:format_ip(Opts)}, handshake)).
+    read(advance(#v1{sock = Sock, proxy = amqpoxy:format_ip(Opts)}, handshake)).
 
 %%
 %% Private
@@ -91,7 +95,7 @@ handle(State = #v1{sock = Sock, stage = handshake}, Data) ->
     case poxy_parser:handshake(Data) of
         {start, Start, Protocol} ->
             reply(Sock, Start, Protocol),
-            next_stage(State#v1{protocol = Protocol}, frame_header);
+            advance(State#v1{protocol = Protocol}, frame_header);
         {refuse, Version, Error} ->
             reply(Sock, Version),
             exit(Error)
@@ -100,7 +104,7 @@ handle(State = #v1{sock = Sock, stage = handshake}, Data) ->
 handle(State = #v1{sock = Sock, stage = frame_header}, Data) ->
     case poxy_parser:header(Data) of
         {info, Info} ->
-            next_stage(State#v1{info = Info}, frame_payload);
+            advance(State#v1{info = Info}, frame_payload);
         {refuse, Version, Error} ->
             reply(Sock, Version),
             exit(Error)
@@ -113,32 +117,26 @@ handle(State = #v1{sock     = Sock,
     case poxy_parser:payload(Info, Data, Protocol) of
         {method, Method} ->
             NewState = method(Method, State),
-            next_stage(NewState#v1{info = undefined}, frame_header);
+            advance(NewState#v1{info = undefined}, frame_header);
         {refuse, Version, Error} ->
             reply(Sock, Version),
             exit(Error)
     end.
 
-next_stage(State, handshake) ->
+advance(State, handshake) ->
     State#v1{stage = handshake, recv_len = ?HANDSHAKE};
-next_stage(State, frame_header) ->
+advance(State, frame_header) ->
     State#v1{stage = frame_header, recv_len = ?HEADER};
-next_stage(State = #v1{info = {_Type, _Channel, Size}}, frame_payload) ->
+advance(State = #v1{info = {_Type, _Channel, Size}}, frame_payload) ->
     State#v1{stage = frame_payload, recv_len = ?PAYLOAD(Size)}.
 
-reply(Sock, Data) ->
-    ok = rabbit_net:send(Sock, Data).
-
-reply(Sock, Command, Protocol) ->
-    ok = rabbit_writer:internal_send_command(Sock, 0, Command, Protocol).
-
-method(#'connection.start_ok'{response = _Response},
+method(#'connection.start_ok'{response = Response},
        State = #v1{sock = Sock, protocol = Protocol}) ->
     Tune = #'connection.tune'{channel_max = 0,
                               frame_max = ?FRAME_MAX_SIZE,
                               heartbeat = 0},
     ok = reply(Sock, Tune, Protocol),
-    State;
+    State#v1{user = decode_user(Response)};
 
 method(#'connection.tune_ok'{frame_max = FrameMax}, State) ->
     if FrameMax /= 0 andalso FrameMax < ?FRAME_MIN_SIZE ->
@@ -151,10 +149,11 @@ method(#'connection.tune_ok'{frame_max = FrameMax}, State) ->
             State
     end;
 
-method(#'connection.open'{virtual_host = _VHostPath},
-       State = #v1{sock = Sock, protocol = Protocol}) ->
+method(#'connection.open'{virtual_host = VHost},
+       State = #v1{sock = Sock, protocol = Protocol, user = User}) ->
+    {ok, Conn} = open_connection('rabbit@13inches', User, VHost),
     ok = reply(Sock, #'connection.open_ok'{}, Protocol),
-    State;
+    State#v1{conn = Conn};
 
 method(#'connection.close'{},
        State = #v1{sock = Sock, protocol = Protocol}) ->
@@ -164,7 +163,60 @@ method(#'connection.close'{},
 method(#'connection.close_ok'{}, State) ->
     State;
 
-method(Command, State) ->
+method(#'channel.open'{},
+       State = #v1{sock = Sock, conn = Conn, chan = undefined, protocol = Protocol}) ->
+    {ok, Chan} = open_channel(Conn),
+    ok = reply(Sock, #'channel.open_ok'{channel_id = <<"0">>}, Protocol),
+    State#v1{chan = Chan};
+method(#'channel.open'{}, State = #v1{sock = Sock, protocol = Protocol}) ->
+    ok = reply(Sock, #'channel.open_ok'{channel_id = <<"0">>}, Protocol),
+    State;
+
+method(#'channel.close'{}, State) ->
+    %% Send close_ok
+    State;
+
+method(#'channel.close_ok'{}, State) ->
+    State;
+
+method(Method, State = #v1{sock = Sock, chan = Chan, protocol = Protocol}) ->
+    Res = amqp_channel:call(Chan, Method),
+    lager:info("forwarding: ~p", [Res]),
+    ok = reply(Sock, Res, Protocol),
+    State.
+
+reply(Sock, Data) ->
+    ok = rabbit_net:send(Sock, Data).
+
+reply(Sock, Command, Protocol) ->
+    ok = rabbit_writer:internal_send_command(Sock, 0, Command, Protocol).
+
+decode_user(Response) ->
+    Table = rabbit_binary_parser:parse_table(Response),
+    {value, {_, _, User}} = lists:keysearch(<<"LOGIN">>, 1, Table),
+    User.
+
+open_connection(Node, User, VHost) ->
+    Params = #amqp_params_direct{username     = User,
+                                 node         = Node,
+                                 virtual_host = VHost},
+    case amqp_connection:start(Params) of
+        {ok, Conn} ->
+            link(Conn),
+            {ok, Conn};
+        Error ->
+            throw(Error)
+    end.
+
+open_channel(Conn) ->
+    case amqp_connection:open_channel(Conn) of
+        {ok, Chan} ->
+            link(Chan),
+            {ok, Chan};
+        Error ->
+            catch amqp_connection:close(Conn),
+            throw(Error)
+    end.
 
 -spec log(atom() | string(), #v1{}) -> ok | {error, lager_not_running}.
 %% @private
