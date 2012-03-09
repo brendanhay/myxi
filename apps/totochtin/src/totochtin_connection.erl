bdabde860b54924a33fd4d854f668241468f06ed
brendanhay <brendan.g.hay@gmail.com>
2012-03-09 17:25:58 +0100

    Switching gen_server->gen_fsm for connections

--- apps/totochtin/src/totochtin_connection.erl
+++ apps/totochtin/src/totochtin_connection.erl
@@ -10,7 +10,7 @@
 
 -module(totochtin_connection).
 
--behaviour(gen_server).
+-behaviour(gen_fsm).
 -behaviour(cowboy_protocol).
 
 -include("include/totochtin.hrl").
@@ -24,11 +24,17 @@
 
 %% Callbacks
 -export([init/1,
-         handle_call/3,
-         handle_cast/2,
-         handle_info/2,
-         terminate/2,
-         code_change/3]).
+         handle_event/3,
+         handle_sync_event/4,
+         handle_info/3,
+         terminate/3,
+         code_change/4]).
+
+%% States
+-export([handshaking/2,
+         handshaking/3,
+         proxying/2,
+         closing/2]).
 
 -record(s, {router   :: router(),
             topology :: ets:tid(),
@@ -39,6 +45,8 @@
             client   :: inet:socket(),
             server   :: inet:socket()}).
 
+-define(TIMEOUT, 1000).
+
 %%
 %% API
 %%
@@ -47,98 +55,120 @@
                 -> {ok, pid()} | ignore | {error, _}.
 %% @doc
 start_link(Listener, Client, cowboy_tcp_transport, Config) ->
-    case gen_server:start_link(?MODULE, {Listener, Client, Config}, []) of
-        {ok, Pid} ->
-            {ok, Pid};
-        Error ->
-            Error
-    end.
+    gen_fsm:start_link(?MODULE, {Listener, Client, Config}, []).
 
 -spec reply(pid(), iolist()) -> ok.
 %% @doc
 reply(Conn, Raw) ->
-    gen_server:cast(Conn, {reply, Raw}).
+    gen_fsm:send_event(Conn, {reply, Raw}).
 
 -spec reply(pid(), pos_integer(), method(), protocol()) -> ok.
 %% @doc
 reply(Conn, Channel, Method, Protocol) ->
-    gen_server:cast(Conn, {reply, Channel, Method, Protocol}).
+    gen_fsm:send_event(Conn, {reply, Channel, Method, Protocol}).
 
 -spec replay(pid(), #'connection.start_ok'{}, iolist(), protocol()) -> ok.
 %% @doc
 replay(Conn, StartOk, Replay, Protocol) ->
-    gen_server:call(Conn, {replay, StartOk, Replay, Protocol}).
+    gen_fsm:sync_send_event(Conn, {replay, StartOk, Replay, Protocol}).
 
 -spec forward(pid(), iolist(), pos_integer(), method(), protocol()) -> ok.
 %% @doc
 forward(Conn, Raw, Channel, Method, Protocol) ->
-    gen_server:cast(Conn, {forward, Raw, Channel, Method, Protocol}).
+    gen_fsm:send_event(Conn, {forward, Raw, Channel, Method, Protocol}).
 
 %%
 %% Callbacks
 %%
 
--spec init(_) -> {ok, #s{}}.
+-spec init(_) -> {ok, idle, #s{}}.
 %% @hidden
 init({Listener, Client, Config}) ->
     process_flag(trap_exit, true),
     totochtin_stats:connect(self()),
-    {ok, #s{router   = totochtin_router:new(Config),
-            listener = Listener,
-            client   = Client}}.
+    {ok, accepting, #s{router   = totochtin_router:new(Config),
+                       listener = Listener,
+                       client   = Client}}.
 
--spec handle_call(_, _, #s{}) -> {reply, ok, #s{}}.
-%% @hidden
-handle_call({replay, StartOk, Replay, Protocol}, {Frontend, _Ref},
-            State = #s{router = Router, topology = Topology, frontend = Frontend}) ->
+handshaking({reply, Channel, Method, Protocol}, State = #s{client = Client}) ->
+    ok = send(Client, Channel, Method, Protocol),
+    {next_state, handshaking, State}.
+
+handshaking({replay, StartOk, Replay, Protocol}, _From,
+          State = #s{router = Router, topology = Topology}) ->
     %% Get a server address according to the routing and relevant balancer
     {Name, Addr, Policies} = totochtin_router:route(Router, StartOk, Protocol),
     %% Start a backend, replaying the previous client data to the server
     {ok, Pid, Sock} = totochtin_backend:start_link(self(), Addr, Replay),
     %% Create a fun composed of all available policies
     Handler = totochtin_policy:handler(Name, Topology, Protocol, Policies),
-    {reply, ok, State#s{backend = Pid,
-                        server  = Sock,
-                        policy  = Handler}}.
+    {reply, ok, proxying, State#s{backend = Pid,
+                                  server  = Sock,
+                                  policy  = Handler}}.
 
--spec handle_cast(_, #s{}) -> {noreply, #s{}}.
-%% @hidden
-handle_cast({reply, Raw}, State = #s{client = Client})
-  when is_port(Client) ->
+proxying({reply, Raw}, State = #s{client = Client}) ->
     ok = send(Client, Raw),
-    {noreply, State};
-handle_cast({reply, Channel, Method, Protocol}, State = #s{client = Client})
-  when is_port(Client) ->
-    ok = send(Client, Channel, Method, Protocol),
-    {noreply, State};
-handle_cast({forward, Raw, Channel, Method, Protocol}, State = #s{server = Server})
-  when is_port(Server) ->
+    {next_state, proxying, State};
+proxying({forward, Raw, Channel, Method, Protocol}, State) ->
     ok = intercept(Raw, Channel, Method, Protocol, State),
-    {noreply, State}.
+    {next_state, proxying, State}.
 
--spec handle_info(_, #s{}) -> {noreply, #s{}} | {stop, normal, #s{}}.
-%% @hidden
-handle_info({shoot, Listener}, State = #s{listener = Listener, client = Client}) ->
-    %% Cowboy acknowledgement
-    lager:info("CONN-ACCEPT"),
-    {ok, Frontend} = totochtin_frontend:start_link(self(), Client),
-    {noreply, State#s{frontend = Frontend}};
-handle_info({'EXIT', Pid, _Msg}, State = #s{frontend = Pid}) ->
+closing(timeout, State) ->
+    lager:notice("FSM-CLOSING-TIMEOUT"),
     {stop, normal, State};
-handle_info({'EXIT', Pid, _Msg}, State = #s{backend = Pid}) ->
-    {stop, normal, State}.
+closing({reply, Raw}, State = #s{client = Client}) ->
+    lager:notice("FSM-SERVER-CLOSE-OK"),
+    ok = send(Client, Raw),
+    {stop, normal, State};
+closing({forward, _Raw, _Channel, Method = #'connection.close_ok'{}, Protocol},
+        State = #s{server = Server}) ->
+    lager:info("FSM-CLIENT-CLOSE-OK"),
+    ok = send(Server, 0, Method, Protocol),
+    {stop, normal, State};
+closing({forward, _Raw, _Channel, _Method, _Protocol}, State) ->
+    {next_state, closing, State, ?TIMEOUT}.
 
--spec terminate(_, #s{}) -> ok.
+handle_event(_Event, StateName, State) ->
+    lager:info("EVENT"),
+    {next_state, StateName, State}.
+
+handle_sync_event(_Event, _From, StateName, State) ->
+    lager:info("SYNC-EVENT"),
+    {reply, ok, StateName, State}.
+
 %% @hidden
-terminate(_Reason, State) ->
-    close_server(State),
-    close_client(State),
-    lager:info("CONN-EXIT"),
+handle_info({'EXIT', Pid, _Msg}, _Any, State = #s{frontend = Pid}) ->
+    ok = close_server(State),
+    lager:info("FRONTEND-EXIT ~p", [Pid]),
+    {next_state, closing, State, ?TIMEOUT};
+handle_info({'EXIT', Pid, _Msg}, _Any, State = #s{backend = Pid}) ->
+    ok = close_client(State),
+    lager:info("BACKEND-EXIT ~p", [Pid]),
+    {next_state, closing, State, ?TIMEOUT};
+
+%% Cowboy acknowledgement
+handle_info({shoot, Listener}, accepting,
+            State = #s{listener = Listener, client = Client}) ->
+    %% Cowboy acknowledgement
+    lager:info("FSM-ACCEPT"),
+    {ok, Frontend} = totochtin_frontend:start_link(self(), Client),
+    {next_state, handshaking, State#s{frontend = Frontend}}.
+
+%% @hidden
+terminate(Reason, _StateName, State = #s{server   = Server,
+                                         backend  = Backend,
+                                         client   = Client,
+                                         frontend = Frontend}) ->
+    lager:error("FSM-EXIT ~p", [Reason]),
+    %% Investigate sockets release before send/2 messages are flushed
+    catch gen_tcp:close(Server),
+    catch gen_tcp:close(Client),
+    catch exit(Backend, kill),
+    catch exit(Frontend, kill),
     ok.
 
--spec code_change(_, #s{}, _) -> {ok, #s{}}.
-%% @hidden
-code_change(_OldVsn, State, _Extra) -> {ok, State}.
+code_change(_OldVsn, StateName, State, _Extra) ->
+    {ok, StateName, State}.
 
 %%
 %% Private
@@ -149,7 +179,7 @@ code_change(_OldVsn, State, _Extra) -> {ok, State}.
 send(Sock, Data) ->
     case gen_tcp:send(Sock, Data) of
         ok    -> ok;
-        Error -> lager:error("CONN-ERR ~p", [Error])
+        Error -> lager:debug("CONN-CLOSED ~p - ~p", [Error, Data])
     end.
 
 -spec send(inet:socket(), non_neg_integer(), method(), protocol()) -> ok.
@@ -157,6 +187,7 @@ send(Sock, Data) ->
 send(Sock, 0, Method, Protocol) ->
     rabbit_writer:internal_send_command(Sock, 0, Method, Protocol);
 send(Sock, Channel, Method, Protocol) ->
+    lager:notice("~p, ~p, ~p, ~p", [Sock, Channel, Method, Protocol]),
     Frame =
         rabbit_binary_generator:build_simple_method_frame(Channel, Method, Protocol),
     send(Sock, Frame).
@@ -177,8 +208,7 @@ intercept(Data, Channel, Method, Protocol, #s{server = Server, policy = Policy})
 -spec close_client(#s{}) -> ok.
 %% @private
 close_client(#s{frontend = Frontend, client = Client}) ->
-    catch exit(Frontend, kill),
-    gen_tcp:close(Client).
+    close(Client).
 
 -spec close_server(#s{}) -> ok.
 %% @private
@@ -188,10 +218,11 @@ close_server(#s{backend = Backend, server = undefined}) ->
     exit(Backend, kill),
     ok;
 close_server(#s{backend = Backend, server = Server}) ->
-    exit(Backend, kill),
+    close(Server).
+
+close(Sock) ->
     Close = #'connection.close'{reply_text = <<"Goodbye">>,
                                 reply_code = 200,
                                 class_id   = 0,
                                 method_id  = 0},
-    rabbit_writer:internal_send_command(Server, 0, Close, rabbit_framing_amqp_0_9_1),
-    gen_tcp:close(Server).
+    send(Sock, 0, Close, rabbit_framing_amqp_0_9_1).
